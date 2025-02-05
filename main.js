f5f4deaf732de6819081e5f1aff0f43e3bc4b230
scree <111024551+screptr@users.noreply.github.com>
2025-02-05 00:38:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const image = document.getElementById('screptyLogo');
+const image = document.getElementsByTagName('screptyLogo');
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
