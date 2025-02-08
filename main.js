a81bdec3a128ce7656e8ae71aa7c315dcc98e73f
scree <111024551+screptr@users.noreply.github.com>
2025-02-08 00:17:28 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
  let hue = 0;
  function animateRainbow() {
         hue = (hue + 1) % 360; // Increment hue (loops 0-360)
-        document.getElementById("rainbowImage").style.filter = `hue-rotate(${hue}deg)`;
+        document.getElementById("rgbLogo").style.filter = `hue-rotate(${hue}deg)`;
         requestAnimationFrame(animateRainbow); // Smooth animation
  }
 
